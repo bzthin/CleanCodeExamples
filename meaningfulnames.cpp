b102aca74901b3c4bc2b7dbc64d215be038c24b5
Bing Zong <bingzong91@hotmail.com>
2019-05-19 22:21:34 +0800

    Adding of Meaningful Names examples

---
+++ meaningfulnames.cpp
@@ -0,0 +1,64 @@
+#include <vector>
+#include <string>
+using namespace std;
+
+// Names should be revealing its intention
+int d; // elapsed time in days
+int elapsedTimeInDays;
+
+// Avoid disinformation 
+vector<int> accountList;
+vector<int> accounts;
+
+int o1;
+int O1;
+int origin;
+int newOrigin;
+
+// Create meaningful distinctions 
+int a1;
+int a2;
+int source;
+int destination;
+
+// Use pronounceable names 
+string ymdhm; // year, month, day, hour, minute
+string timeStamp;
+
+// Use Searchable Names 
+int number;
+int numberOfStudents;
+
+// Use encoding when appropriate
+int count; // global variable
+int gCount;
+
+struct Number {};
+Number phoneString; // Forgot to update the name!
+Number phoneNumber;
+
+// Avoid internal joke names
+bool holyHandGrenade;
+bool shutdownSystemFlag;
+
+// Use domain names 
+bool IsSimilarWhenReversed(string input);
+bool IsPalindrome(string input);
+
+// Remain consistent with concepts 
+string GetName();
+string FetchAddress();
+string RetrieveUnit();
+string GetName();
+string GetAddress();
+string GetUnit();
+
+// Add Meaningful Context  
+string name;
+string city;
+string state;
+
+Address addr;
+addr.name;
+addr.city;
+addr.state;
