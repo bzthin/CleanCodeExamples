4175b6db60c54eda3e30eea40371b699ac420c1d
Bing Zong <bingzong91@hotmail.com>
2019-05-20 23:39:13 +0800

    Add easy to read formatting examples

---
+++ easytoreadformatting.cpp
@@ -0,0 +1,51 @@
+// Easy To Read Formatting //
+// Vertical spacing
+void PrepareBreakfast()
+{
+  TakeEgg();
+  BoilEgg();
+  ServeEggs();
+  TakeHotdog();
+  FryHotdog();
+  ServeHotdog();
+  TakeBread();
+  ToastBread();
+  ServeBread();
+}
+
+void PrepareBreakfast()
+{
+  TakeEgg();
+  BoilEgg();
+  ServeEggs();
+
+  TakeHotdog();
+  FryHotdog();
+  ServeHotdog();
+
+  TakeBread();
+  ToastBread();
+  ServeBread();
+}
+
+// Horizontal formatting: Horizontal spacing
+float determinant(float a, float b, float c)
+{
+  return b*b-4.0*a*c;
+}
+
+// Horizontal formatting: Horizontal alignment 
+int                   count;
+InputStream           input;
+OutputStream          output;
+bool                  errorFlag;
+WorldRenderingManager renderManager;
+
+int count;
+InputStream input;
+OutputStream output;
+bool errorFlag;
+WorldRenderingManager renderManager;
+
+
+/////////////////////////////
